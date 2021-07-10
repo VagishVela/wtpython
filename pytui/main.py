8eee1ffb0ddb0df7c03ec2cd15129f372a0f7ab5
Ethan <Ethanparkerhenry@gmail.com>
2021-07-09 18:02:52 -0700

    Changes that should hopefully fix the flake8 errors

--- pytui/main.py
+++ pytui/main.py
@@ -1,20 +1,19 @@
 import sys
 
-from parsers.python import PythonParser
 from backends.stackoverflow import StackOverflowFinder
 from display.display import Display
+from parsers.python import PythonParser
 
 
-def main():
+def main() -> None:
+    """Run the application"""
     all_text = sys.stdin.read()
 
     parser = PythonParser(all_text)
     error_message = parser.parse_error()
-    
     backend = StackOverflowFinder()
-    search_result = backend.search(error_message)
-    
-    Display(error_message).start()
+    search_results = backend.search(error_message)
+    Display(error_message, search_results).start()
 
 
 if __name__ == "__main__":
