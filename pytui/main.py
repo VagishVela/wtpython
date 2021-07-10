ac30495ae639089da3d3052f8666080cfafb1125
Brian Cohan <briancohan@gmail.com>
2021-07-10 15:06:02 -0400

    Parsing stderr and identifying the key parts of the error.

--- pytui/main.py
+++ pytui/main.py
@@ -1,19 +1,47 @@
+import subprocess  # noqa: S404
 import sys
 
-from backends.stackoverflow import StackOverflowFinder
-from display.display import Display
-from parsers.python import PythonParser
+from parse import findall
+from rich import print
+
+
+def parse_err(txt: str) -> tuple:
+    """Extract error message and packages invloved in stack trace.
+
+    The specific error message is assumed to be the last line that is not indented.
+    """
+    error_msg = ''
+    for line in txt.splitlines():
+        if line == line.strip():
+            error_msg = line.strip()
+
+    files = [
+        line['file']
+        for line
+        in findall('File "{file}", line {line_no}, in {method}', txt)
+    ]
+
+    packages = set()
+    key = 'site-packages'
+    for file in files:
+        if key not in file:
+            continue
+        path_elements = file.split('/')
+        packages.add(path_elements[path_elements.index(key) + 1])
+
+    return error_msg, packages
 
 
 def main() -> None:
     """Run the application"""
-    all_text = sys.stdin.read()
+    args = sys.argv
+    args[0] = 'python'  # Replace pytui with 'python' to act as a runner
+    result = subprocess.call(args, stderr=subprocess.PIPE, shell=False)  # noqa: S603
 
-    parser = PythonParser(all_text)
-    error_message = parser.parse_error()
-    backend = StackOverflowFinder()
-    search_results = backend.search(error_message)
-    Display(error_message, search_results).start()
+    if result.stderr:
+        error_msg, packages = parse_err(result.stderr.decode('utf-8'))
+        print(f"[red bold]Error Message:[/] {error_msg}")
+        print(f"[blue bold]Packages:[/] {', '.join(packages)}")
 
 
 if __name__ == "__main__":
