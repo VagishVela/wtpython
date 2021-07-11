baa69421c18dc13f52f24fb45b956ffb89e3b11f
Brian Cohan <briancohan@gmail.com>
2021-07-11 10:18:25 -0400

    minimal integration showing stack overflow result count

--- pytui/display/display.py
+++ pytui/display/display.py
@@ -3,6 +3,7 @@ from textual.app import App
 from textual.view import DockView
 from textual.widgets import Footer, Header, Placeholder, ScrollView
 
+from pytui.backends.stackoverflow import StackOverflowFinder
 from pytui.parsers.parse_errors import run_and_get_errors
 
 
@@ -18,11 +19,15 @@ class Display(App):
         """App layout"""
         self.error, self.packages = run_and_get_errors()
         view = await self.push_view(DockView())
-        header = Header("pytui")
+
+        stack_overflow = StackOverflowFinder()
+        error_answers = stack_overflow.search(self.error, 10)  # noqa: F841
+
+        header = Header(f"PyTUI: {self.error}")
         footer = Footer()
         sidebar = Placeholder(name="sidebar")
 
-        body = ScrollView(self.error)
+        body = ScrollView(f"We found {len(error_answers)} answers")
 
         footer.add_key("b", "Toggle sidebar")
         footer.add_key("q", "Quit")
