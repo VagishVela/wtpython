6026d364647178eeff9ab139ae8664d11f1c9274
Ethan <Ethanparkerhenry@gmail.com>
2021-07-10 23:40:35 -0700

    Fixed

--- pytui/display/display.py
+++ pytui/display/display.py
@@ -3,24 +3,26 @@ from textual.app import App
 from textual.view import DockView
 from textual.widgets import Footer, Header, Placeholder, ScrollView
 
+from pytui.parsers.parse_errors import run_and_get_errors
+
 
 class Display(App):
     """An example of a very simple Textual App"""
 
     async def on_load(self, event: events.Load) -> None:
-        """Navifation setup for display"""
+        """Navigation setup for display"""
         await self.bind("q,ctrl+c", "quit")
         await self.bind("b", "view.toggle('sidebar')")
 
     async def on_startup(self, event: events.Startup) -> None:
         """App layout"""
+        self.error, self.packages = run_and_get_errors()
         view = await self.push_view(DockView())
         header = Header(self.title)
         footer = Footer()
         sidebar = Placeholder(name="sidebar")
 
-        readme = "test"
-        body = ScrollView(readme)
+        body = ScrollView(self.error)
 
         footer.add_key("b", "Toggle sidebar")
         footer.add_key("q", "Quit")
