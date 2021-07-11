8d8c3a43566f3bdda90245586bcc98d5476442ba
Mustafa Quraish <mustafaquraish@gmail.com>
2021-07-11 13:57:56 -0400

    Added intermediary `get_all_error_results()` function
    Since we can't pass any information into the Display class, but for
    debugging purposes we also want to be able to test the program
    without opening up the display, this commit adds the above function
    where all the core logic has been added (and taken out of the
    Display class)
    
    Additionally, the `main()` function now checks for a `-n` flag, and
    if it exists, the display is never started and the function is called
    directly so any printed output can be seen.
    
    If the flag does not exist, the Display is started. Internally, the
    Display class also calls this function.

--- pytui/__main__.py
+++ pytui/__main__.py
@@ -1,15 +1,27 @@
 import sys
 
+from pytui.core import get_all_error_results
 from pytui.display import Display
 
 
 def main() -> None:
     """Run the application"""
+    no_display = False
+
+    if len(sys.argv) > 1 and sys.argv[1] == '-n':
+        no_display = True
+        sys.argv.pop(1)
+
     if len(sys.argv) == 1:
-        print("Usage: pytui <script>")
+        print("Usage: pytui [-n] <script>")
         sys.exit(1)
 
-    Display().run()
+    if no_display:
+        results = get_all_error_results()
+        print(results)
+
+    else:
+        Display().run()
 
 
 if __name__ == "__main__":
