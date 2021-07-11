c78af9d29508c8ff0428ba20625f0b2e3f5ce364
Mustafa Quraish <mustafaquraish@gmail.com>
2021-07-11 12:03:02 -0400

    Change `main.py` to `__main__.py`
    This is the recommended way to have a main entrypoint in a packaged
    executable. This also allows the user to run `python -m pytui ...`

--- pytui/__main__.py
+++ pytui/__main__.py
@@ -1,8 +1,14 @@
+import sys
+
 from pytui.display import Display
 
 
 def main() -> None:
     """Run the application"""
+    if len(sys.argv) == 1:
+        print("Usage: pytui <script>")
+        sys.exit(1)
+
     Display().run()
 
 
