e41718698f89d5593a96d0a49dbfc55294ebd934
Brian Cohan <briancohan@gmail.com>
2021-07-11 21:24:49 -0400

    refactored parsing stacktrace

--- pytui/parser.py
+++ pytui/parser.py
@@ -1,29 +1,47 @@
+from typing import Any
+
 from parse import findall
 
 
-def parse_error(txt: str) -> tuple:
-    """
-    Extract error message and packages involved in stack trace.
-
-    The specific error message is assumed to be the last line that is not indented.
-    """
-    error_msg = ''
+def _error_message(txt: str) -> str:
+    """The specific error message is assumed to be the last line that is not indented."""
+    error_message = ''
     for line in txt.splitlines():
         if line == line.strip():
-            error_msg = line.strip()
+            error_message = line.strip()
+    return error_message
 
-    files = [
-        line['file']
+
+def _error_files(txt: str) -> list[dict[str, str]]:
+    """Extract files named in stack trace."""
+    return [
+        line.named
         for line
-        in findall('File "{file}", line {line_no}, in {method}', txt)
+        in findall('File "{file}", line {line}, in {method}\n', txt)
     ]
 
+
+def _extract_packages(files: list[dict[str, str]]) -> set[str]:
+    """Extract file info from each file reference."""
     packages = set()
     key = 'site-packages'
-    for file in files:
+    for file_info in files:
+        file = file_info['file']
         if key not in file:
             continue
         path_elements = file.split('/')
         packages.add(path_elements[path_elements.index(key) + 1])
+    return packages
 
-    return error_msg, packages
+
+def parse_stacktrace(txt: str) -> dict[str, Any]:
+    """Extract error message and packages involved in stack trace."""
+    error_message = _error_message(txt)
+    files = _error_files(txt)
+    packages = _extract_packages(files)
+
+    return {
+        'error_message': error_message,
+        'files': files,
+        'packages': packages,
+    }
--- tests/test_error_parsing.py
+++ tests/test_error_parsing.py
@@ -1,7 +1,7 @@
 import pytest
 import toml
 
-from pytui.parse_errors import parse_err
+from pytui.parser import parse_stacktrace
 
 
 @pytest.fixture
@@ -11,13 +11,30 @@ def sample_error(shared_datadir: pytest.fixture) -> dict:
         yield toml.load(file)
 
 
+def test_parsing_has_correct_format(sample_error: pytest.fixture) -> None:
+    """Ensure parse_stacktrace provides dict."""
+    parsed = parse_stacktrace(sample_error['traceback'])
+    assert list(parsed.keys()) == ['error_message', 'files', 'packages']  # noqa: S101
+    for value in parsed.values():
+        assert value  # noqa: S101
+
+
 def test_correct_error_message(sample_error: pytest.fixture) -> None:
     """Ensure that the error message is correctly capturerd."""
-    error_msg, _ = parse_err(sample_error['traceback'])
-    assert error_msg == sample_error['error_message']  # noqa: S101
+    parsed = parse_stacktrace(sample_error['traceback'])
+    assert parsed['error_message'] == sample_error['error_message']  # noqa: S101
+
+
+def test_correct_file_references(sample_error: pytest.fixture) -> None:
+    """Ensure file information was extracted correctly."""
+    parsed = parse_stacktrace(sample_error['traceback'])
+    for field in ['file', 'line', 'method']:
+        found = [i[field] for i in parsed['files']]
+        expected = [i[field] for i in sample_error['files']]
+        assert found == expected  # noqa: S101
 
 
 def test_correct_packages(sample_error: pytest.fixture) -> None:
     """Ensure that packages involved are correrctly identified."""
-    _, packages = parse_err(sample_error['traceback'])
-    assert list(packages) == list(sample_error['error_packages'])  # noqa: S101
+    parsed = parse_stacktrace(sample_error['traceback'])
+    assert set(parsed['packages']) == set(sample_error['packages'])  # noqa: S101
