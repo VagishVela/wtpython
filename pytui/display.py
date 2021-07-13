73b8da87695c3756fcdbaa4097825acdfa968b27
Ethan <Ethanparkerhenry@gmail.com>
2021-07-13 10:25:36 -0700

    Sidebar

--- pytui/display.py
+++ pytui/display.py
@@ -1,15 +1,48 @@
 import webbrowser
+from typing import List, Union
 
 import markdownify
+from rich import box
+from rich.align import Align
 from rich.console import RenderableType
 from rich.markdown import Markdown
+from rich.panel import Panel
 from textual import events
 from textual.app import App
 from textual.views import DockView
-from textual.widgets import Footer, Header, Placeholder, ScrollView
+from textual.widget import Widget
+from textual.widgets import Footer, Header, ScrollView
 
 from pytui.backends.stackoverflow import StackOverflowQuestion
 from pytui.core import get_all_error_results
+
+
+class Sidebar(Widget):
+    """Sidebar with list of questions and possible answers"""
+
+    def __init__(self, name: Union[str, None], questions: List[StackOverflowQuestion] = None) -> None:
+        if questions is not None:
+            self.questions = questions
+        self.index = None
+        super().__init__(name=name)
+
+    def get_questions(self) -> str:
+        """Put questions into legible format"""
+        text = ""
+        for i, question in enumerate(self.questions):
+            text += f"#{i + 1} - "+question.title + "\n\n"
+        return text
+
+    def render(self) -> RenderableType:
+        """Render the panel"""
+        return Panel(
+            Align.center(
+                self.get_questions(), vertical="top"
+            ),
+            title="Questions",
+            border_style="blue",
+            box=box.ROUNDED,
+        )
 
 
 class Display(App):
@@ -19,8 +52,8 @@ class Display(App):
         """Navigation setup for display"""
         await self.bind("q,ctrl+c", "quit")
         await self.bind("b", "view.toggle('sidebar')")
-        await self.bind("up", "prev_question")
-        await self.bind("down", "next_question")
+        await self.bind("left", "prev_question")
+        await self.bind("right", "next_question")
         await self.bind("o", "open_browser")
 
     def create_body_text(self) -> RenderableType:
@@ -58,7 +91,7 @@ class Display(App):
     async def action_open_browser(self) -> None:
         """Open the question in the browser"""
         if self.data["results"] != []:
-            webbrowser.open(self.data["results"][self.index].url)
+            webbrowser.open(self.data["results"][self.index].link)
 
     async def on_startup(self, event: events.Startup) -> None:
         """App layout"""
@@ -67,11 +100,14 @@ class Display(App):
         self.data = get_all_error_results()
         header = Header()
         footer = Footer()
-        self.sidebar = Placeholder(name="sidebar")
+        self.sidebar = Sidebar("Sidebar", self.data["results"])
         self.body = ScrollView(self.create_body_text())
 
         footer.add_key("b", "Toggle sidebar")
         footer.add_key("q", "Quit")
+        footer.add_key("o", "Open question in browser")
+        footer.add_key("<-", "Previous question")
+        footer.add_key("->", "Next question")
 
         await view.dock(header, edge="top")
         await view.dock(footer, edge="bottom")
