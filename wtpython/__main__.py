091abb55758f5c39aa5bea9a16aafe3fe15ea021
Brian Cohan <briancohan@gmail.com>
2021-07-16 14:26:09 -0400

    show error and request user to open a new issue if the app crashes.

--- wtpython/__main__.py
+++ wtpython/__main__.py
@@ -9,7 +9,7 @@ from rich import print
 from wtpython import SearchError
 from wtpython.backends.stackoverflow import StackOverflowFinder
 from wtpython.display import Display, store_results_in_module
-from wtpython.settings import MAX_SO_RESULTS
+from wtpython.settings import GH_ISSUES, MAX_SO_RESULTS
 
 
 def run(args: list[str]) -> Exception:
@@ -24,6 +24,15 @@ def run(args: list[str]) -> Exception:
     finally:
         sys.argv = stashed
     return exc
+
+
+def display_app_error(exc: Exception) -> None:
+    """Display error message and request user to report an issue."""
+    print(":cry: We're terribly sorry, but our app has encounterd an issue.")
+    print("-" * 80)
+    traceback.print_exception(type(exc), exc, exc.__traceback__)
+    print("-" * 80)
+    print(f":nerd_face: Please let us know by by opening a new issue at {GH_ISSUES}")
 
 
 def parse_arguments() -> tuple[dict, list]:
@@ -66,7 +75,7 @@ def main() -> None:
     try:
         so_results = so.search(error, MAX_SO_RESULTS)
     except SearchError as e:
-        print(e)
+        display_app_error(e)
         return
 
     if flags['no_display']:
@@ -74,7 +83,11 @@ def main() -> None:
         print(so_results)
     else:
         store_results_in_module(exc, so_results)
-        Display().run()
+        try:
+            Display().run()
+        except Exception as e:
+            display_app_error(e)
+            return
 
 
 if __name__ == "__main__":
