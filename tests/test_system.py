f151b521f3585a9af1b5046033e8e52b1d045c57
Brian Cohan <briancohan@gmail.com>
2021-07-17 10:19:41 -0400

    changed to version 0.1

--- tests/test_system.py
+++ tests/test_system.py
@@ -3,4 +3,4 @@ import wtpython
 
 def test_version() -> None:
     """Test Version is Correct"""
-    assert wtpython.__version__ == "0.0.1"  # noqa: S101
+    assert wtpython.__version__ == "0.1"  # noqa: S101
